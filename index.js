827548a7a7ebe0546224a9a09429607e08866fd0
EduuA <devsouza.exe@gmail.com>
2024-05-02 00:46:03 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log("Meu repositorio no git");
 console.log("Meu repositorios no git 2");
+
+//variaveis
+const primeiroNome = "Eduardo";
+
+console.log(primeiroNome);
+console.log("Volta ao normal, porfvaor");
